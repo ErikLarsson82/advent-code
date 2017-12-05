31fa5a319934d0b4036f60b256c1bcc0ab016a42
Erik Larsson <erik.mattias.larsson@gmail.com>
2017-12-05 10:20:20 +0100

    Part 2 code solves part 1 correctly now

--- day3_part2.js
+++ day3_part2.js
@@ -16,47 +16,41 @@ function setValue(value) {
   return matrix[pos.x][pos.y] = value
 }
 
-const up = (function() {
-  return () => pos.y -= 1
-})()
+const up = function() {
+  return pos.y -= 1
+}
 
-const down = (function() {
-  return () => pos.y += 1
-})()
+const down = function() {
+  return pos.y += 1
+}
 
-const left = (function() {
-  return () => pos.x -= 1
-})()
+const left = function() {
+  return pos.x -= 1
+}
 
-const right = (function() {
-  return () => pos.x += 1
-})()
+const right = function() {
+  return pos.x += 1
+}
 
 function crawl(goal) {
   let iterations = 1
   while(true) {
     grow(matrix)
 
-    //console.log(matrix)
-    //console.log('pos', pos)
-
     let sequence = []
-    if (iterations === 1)
-      sequence = [right, up, left, left, down, down, right, right]
-    if (iterations === 2)
-      sequence = [right, up, up, up, left, left, left, left, down, down, down, down, right, right, right, right]
-    if (iterations === 3)
-      sequence = [right, up, up, up, up, up, left, left, left, left, left, left, down, down, down, down, down, down, right, right, right, right, right, right]
+    sequence.push(right)
+
+    new Array(matrix.length - 2).fill(1).forEach( () => sequence.push(up) )
+    new Array(matrix.length - 1).fill(1).forEach( () => sequence.push(left) )
+    new Array(matrix.length - 1).fill(1).forEach( () => sequence.push(down) )
+    new Array(matrix.length - 1).fill(1).forEach( () => sequence.push(right) )
 
     while(sequence.length > 0) {
       value ++
       setValue(value)
-      //console.log('compare ' +  getValue() + " " + goal)
       if (getValue() === goal) {
-        //console.log('found goal', pos, matrix)
         const xDiff = pos.x - Math.floor(matrix[0].length/2)
         const yDiff = pos.y - Math.floor(matrix[0].length/2)
-        //console.log('xDiff, yDiff ' + xDiff + " : " + yDiff)
         return Math.abs(xDiff) + Math.abs(yDiff)
       }
       sequence.shift()()
@@ -77,33 +71,6 @@ function reset() {
 }
 
 reset()
-console.log(crawl(20))
-/*
+console.log(crawl(1024)) //31
 reset()
-console.log(crawl(3, 1))
-reset()
-console.log(crawl(3, 2))
-reset()
-console.log(crawl(3, 3))
-reset()
-console.log(crawl(3, 4))
-reset()
-console.log(crawl(3, 5))
-reset()
-console.log(crawl(3, 6))
-reset()
-console.log(crawl(3, 7))
-reset()
-console.log(crawl(3, 8))
-reset()
-console.log(crawl(3, 9))
-reset()
-console.log(crawl(3, 10))
-reset()
-console.log(crawl(3, 11))
-reset()
-console.log(crawl(3, 12))
-reset()
-console.log(crawl(3, 13))
-*/
-console.log(matrix)
+console.log(crawl(368078)) //371
