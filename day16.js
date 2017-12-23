04c2333f1db1c9d2e75b3f1c0f63c4c4222c8822
Erik Larsson <erik.mattias.larsson@gmail.com>
2017-12-23 23:49:13 +0100

    I completely slaughtered the code for optimization purposes

--- day16.js
+++ day16.js
@@ -3,15 +3,21 @@ const fs = require('fs')
 const contentStr = fs.readFileSync('day16_input.txt', 'utf-8')
 
 function dance(str, amount = 1) {
-  let list = "abcdefghijklmnop".split("")
+  let list = "abcdefghijklmnop"
 
   const instructionStrings = str.trim().split(",").map( x => x.trim() )
 
   const instructions = instructionStrings.map( translateInstruction )
 
-  times( () => {
+  /*times( () => {
     list = instructions.reduce( (acc, curr) => curr(acc), list )
-  }, amount)
+  }, amount)*/
+  while(amount > 0) {
+    amount--
+    for ( var i = 0; i < instructions.length; i++) {
+      list = instructions[i](list)
+    }
+  }
 
   return list
   //const loop = new Array(amount).fill(1)
@@ -47,19 +53,40 @@ function translatePartner(str) {
 const spin = curry((size, list) => {
   while(size > 0) {
     size--
-    const endBit = list[list.length-1]
-    list = list.slice(0, list.length-1)
-    list.unshift(endBit)
+    list = list[list.length-1] + list.substring(0, list.length-1)
   }
   return list
 })
 
-const exchange = curry((x, y, list) => {
-  const first = list[x]
-  const second = list[y]
-  list[x] = second
-  list[y] = first
-  return list
+const exchange = curry((x, y, str) => {
+  //console.log('one list\n', x,y,str)
+  if (x > y) {
+    let tmp = x
+    x = y
+    y = tmp
+  }
+  if (x === y)
+    return str
+  //console.log('wat', x,y,str)
+  
+  return str.substr(0, x) + str[y] + str.substring(x+1, y) + str[x] + str.substr(y+1)
+  
+  const one = str.substr(0, x)
+  const two = str[y]
+  const three = str.substring(x+1, y)
+  const four = str[x]
+  const five = str.substr(y+1)
+  const result = one + two + three + four + five
+  //return result
+  
+  //console.log(one)
+  //  console.log(two)
+  //  console.log(three)
+ // //    console.log(four)
+ //     console.log(five)
+ // console.log(result)
+  //if (result.length !== str.length)
+  //  while(true) {}
 })
 
 
@@ -71,7 +98,7 @@ const partner = curry((x, y, list) => {
 
 console.time('dance')
 
-dance(contentStr, 1).join("")
+dance(contentStr, 1000)
 
 console.timeEnd('dance')
 
--- test/day16.js
+++ test/day16.js
@@ -8,11 +8,11 @@ function compare(a, b) {
 }
 
 describe('dance', () => {
-  it('dances', () => assert( dance(contentStr).join("") === "cknmidebghlajpfo") )
-  it.only('dances on BILLION times', () => console.log(dance(contentStr, 1000000000).join("")) )
+  //it('dances', () => assert( dance(contentStr) === "cknmidebghlajpfo") )
+  //it.only('dances on BILLION times', () => console.log(dance(contentStr, 1000000000).join("")) )
 })
 
-describe('spin', () => {
+/*describe('spin', () => {
   it('spins', () => assert(compare( spin(3, ["a","b","c","d","e"] ), ["c","d","e","a","b"] )))
   it('spins', () => assert(compare( spin(1, ["a","b","c","d","e"] ), ["e","a","b","c","d"] )))
 })
@@ -26,4 +26,22 @@ describe('exchange', () => {
 describe('partner', () => {
   it('partners', () => assert(compare( partner("b", "d", "abcde".split("") ), "adcbe".split("") )))
   it('partners', () => assert(compare( partner("a", "e", "abcde".split("") ), "ebcda".split("") )))
+})*/
+
+
+describe('spin', () => {
+  it('spins', () => assert(compare( spin(3, "abcde" ), "cdeab" )))
+  it('spins', () => assert(compare( spin(1, "abcde" ), "eabcd" )))
+})
+
+describe('exchange', () => {
+  it('exchanges', () => assert(compare( exchange(3, 4, "eabcd" ), "eabdc" )))
+  it('exchanges', () => assert(compare( exchange(0, 1, "abcde" ), "bacde" )))
+  it('exchanges', () => assert(compare( exchange(10, 6, "abcdefnhijklmgop" ), "abcdefkhijnlmgop" )))
+})
+
+
+describe('partner', () => {
+  it('partners', () => assert(compare( partner("b", "d", "abcde" ), "adcbe" )))
+  it('partners', () => assert(compare( partner("a", "e", "abcde" ), "ebcda" )))
 })
