6a03ec7d2e1973d6e82ea32913fde6fa53119882
Erik Larsson <erik.mattias.larsson@gmail.com>
2018-07-04 09:38:24 +0200

    Lots of trying on day 23 but no progress

--- day_23.js
+++ day_23.js
@@ -1,7 +1,7 @@
 const puzzleInput = require('./day_23_input.json')
 const splitInput = puzzleInput.map(x => x.split(" "))
 
-const registers = {
+/*const registers = {
   a: 1,
   b: 0,
   c: 0,
@@ -10,13 +10,134 @@ const registers = {
   f: 0,
   g: 0,
   h: 0
+}*/
+/*
+//First skip
+const registers = {
+  a: 1,
+  b: 107900,
+  c: 124900,
+  d: 2,
+  e: 53950,
+  f: 1,
+  g: 0,
+  h: 0
+}*/
+/*
+//Second skip
+const registers = {
+  a: 1,
+  b: 107900,
+  c: 124900,
+  d: 2,
+  e: 107900,
+  f: 0,
+  g: 0,
+  h: 0
+}*/
+/*
+//Third skip
+const registers = {
+  a: 1,
+  b: 107917,
+  c: 124900,
+  d: 107900,
+  e: 107900,
+  f: 1,
+  g: -17000,
+  h: 1
+}*/
+/*
+//Fourth, when f goes from 1 to 0 again
+const registers = {
+  a: 1,
+  b: 107917,
+  c: 124900,
+  d: 311,
+  e: 347,
+  f: 0,
+  g: 0,
+  h: 1
 }
+*/
+/*//Fifth, when f goes from 1 to 0 again
+const registers = {
+  a: 1,
+  b: 107917,
+  c: 124900,
+  d: 311,
+  e: 1302+106615,
+  f: 0,
+  g: 0,
+  h: 1
+}*/
+
+/*//sex, when f goes from 1 to 0 again
+const registers = {
+  a: 1,
+  b: 107917,
+  c: 124900,
+  d: 312,
+  e: 107917,
+  f: 0,
+  g: 0,
+  h: 1
+}*/
+//sju
+/*const registers = {
+  a: 1,
+  b: 107917,
+  c: 124900,
+  d: 312,
+  e: 478+107438,
+  f: 0,
+  g: 0,
+  h: 1
+}*/
+//åtta, g ? needs to be 0 to skip but is -16983 idx 28
+const registers = {
+  a: 1,
+  b: 107917,
+  c: 107917, //124900
+  d: 312,
+  e: 107916,
+  f: 0,
+  g: -16983,
+  h: 2
+}
+/*
+//This was once a baseline, starting at 19
+const registers = {
+  a: 1,
+  b: 107900,
+  c: 124900,
+  d: 107898,
+  e: 107900,
+  f: 0,
+  g: 0,
+  h: 0
+}*/
+
+/*
+// This turned out to be a dead end
+const registers = {
+  a: 1,
+  b: 107917,
+  c: 124900,
+  d: 107900,
+  e: 107900,
+  f: 0,
+  g: -17000,
+  h: 1
+}*/
 
 let mulInstructionsRun = 0
 
-let currentInstructionIdx = 0
+let currentInstructionIdx = 28
 
 let counter = 0
+
+let terminate = false
 
 //Try to see if this is a register
 function registerOrValue(input) {
@@ -25,13 +146,16 @@ function registerOrValue(input) {
 }
 
 function run() {
-  while(splitInput[currentInstructionIdx] !== undefined) {
+  while(splitInput[currentInstructionIdx] !== undefined && terminate === false) {
     counter++
-    if (counter % 100000000 === 0)
+    if (counter % 10000000 === 0)
       console.log('iteration', counter, registers)
 
     const currentInstruction = splitInput[currentInstructionIdx][0]
     const targetRegister = splitInput[currentInstructionIdx][1]
+
+    //if (targetRegister === undefined)
+    //  console.log()
     const targetValue = registerOrValue(splitInput[currentInstructionIdx][2])
 
     //console.log('Instruction: ', splitInput[currentInstructionIdx])
@@ -40,8 +164,18 @@ function run() {
     //console.log('Value', targetValue, typeof targetValue)
     //console.log('\n')
 
+    /*if (registers.d !== 312) {
+      console.log('Register D warning', registers.d)
+      console.log(registers, currentInstructionIdx)
+      terminate = true
+      return
+    }*/
+    if (targetRegister === "d") {
+      //console.log('im gonna manipulate d', currentInstruction, targetValue, splitInput[currentInstructionIdx][2])
+      //console.log('registers before', registers)
+    }
     switch(currentInstruction) {
-      case "set": 
+      case "set":
         registers[targetRegister] = targetValue
         currentInstructionIdx++
         break;
@@ -55,13 +189,48 @@ function run() {
         mulInstructionsRun++
         break;
       case "jnz":
+        if (currentInstructionIdx === 28) {
+          console.log('status at 28')
+          console.log(registers)
+        }
         if (registers[targetRegister] === 0) {
+          if (currentInstructionIdx !== 19 && currentInstructionIdx !== 14) {
+            console.log(`skip jump!! goto next at ${registers[targetRegister]} idx ${currentInstructionIdx}`)
+            console.log(registers)
+            //terminate = true
+            //return
+          }
           currentInstructionIdx++
         } else {
+          if (currentInstructionIdx === 28) {
+              console.log(`who needs to be 0 to skip but is ${registers[targetRegister]} idx ${currentInstructionIdx}`)
+              console.log(registers)
+          }
+          //console.log(`performing -- because ${registers[targetRegister]}, idx ${currentInstructionIdx}, target ${targetRegister}`)
+          //console.log(registers)
+          /*if (currentInstructionIdx !== 14) {
+            if (registers[targetRegister] > -10) {
+              console.log('regs', registers)
+            }
+            if (registers[targetRegister] === -1700) {
+              console.log('regs2', registers)
+            }
+            if (registers[targetRegister] === undefined) {
+              console.log('regs3', registers)
+            }
+            if (registers[targetRegister] > -107914 && registers[targetRegister] < -107910) {
+              console.log('regs4', registers)
+            }
+            if (targetRegister === "g") {
+              console.log('regs5', registers)
+            }
+          }*/
           currentInstructionIdx = currentInstructionIdx + targetValue
         }
         break;
     }
+    //if (targetRegister === "d")
+      //console.log('registers after', registers)
   }
 }
 
