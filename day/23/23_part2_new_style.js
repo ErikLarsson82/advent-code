404b7e5a206af9ac759018ee85a3af6faf19c97c
Erik Larsson <erik.mattias.larsson@gmail.com>
2018-07-05 10:47:19 +0200

    Day 23 part 2 solved

--- day/23/23_part2_new_style.js
+++ day/23/23_part2_new_style.js
@@ -3,43 +3,23 @@ var a = 1, b, c, d, e, f, h = 0
 b = 107900
 c = 124900
 
-let count = 0
-for (var x = 107900; x < 124900; x = x + 17) {
-  let prime = true
-  for (var y = 2; y < x; y++) {
-    if (x % y === 0) {
-      prime = false
-    }
-  }
-  if (prime)
-    count++
-}
-console.log(count)
-return
-
-while (true) {
+do {
 
   f = true
-  d = 2
-  do {
-    e = 2
-    do {
-      if ((d * e) - b === 0) {
-        f = false
-      }
-      e = e + 1
-    } while(e - b !== 0)
-
-    d = d + 1
-
-  } while(d - b !== 0)
-
-  if (f === true) {
-    h = h + 1
+  for (d = 2; d < b; d += 1) {
+    if (b % d === 0) {
+      f = false
+      break;
+    }
   }
-  if (b - c === 0) {
-    console.log('h', h)
-    return
+
+  if (!f) {
+    h++
   }
+  if (b - c === 0) { break }
+
   b = b + 17
-}
+
+} while (true)
+
+console.log('h', h)
