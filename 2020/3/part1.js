7d25f2f9604001623a4f948ccbd1af3398524b9f
Erik Larsson <erik.mattias.larsson@gmail.com>
2020-12-27 14:18:53 +0100

    Solved

---
+++ 2020/3/part1.js
@@ -0,0 +1,96 @@
+const { data } = require('../common')
+const { pipe } = require('ramda')
+
+require('fs').readFile('./puzzle-input.txt', 'utf-8', pipe(data, parseWires, crossWires))
+
+// takes a string input and turs into an array of arrays with instructions
+// "R8,U5,L5,D3\nU7,R6,D4,L4" -> [['R8','U5','L5','D3'],['U7','R6','D4','L4']]
+function parseWires(data) {
+	return data.trim()
+      .split('\n')
+      .map(x => x.trim())
+      .map(wireString => wireString.split(','))
+}
+
+const circuitBoard = []
+
+const directions = {
+	R: { x: 1, y: 0 },
+	L: { x: -1, y: 0 },
+	U: { x: 0, y: -1 },
+	D: { x: 0, y: 1 }
+}
+
+function crossWires(wires) {
+
+	let target
+
+	wires.forEach((wire, wireIdx) => {
+		console.log('Running wire', wireIdx)
+		target = reset()
+		wire.forEach((instruction, instructionIdx, instructionArray) => {
+			console.log('Running instruction', instruction, instructionIdx + " / " + instructionArray.length)
+			const direction = instruction[0]
+			const steps = parseInt(instruction.substring(1))
+
+			for (var i = 0; i < steps; i++) {
+				const { x, y } = directions[direction] 
+				target.x += x
+				target.y += y
+				increment({ ... target})
+			}
+		})
+	})
+
+	console.log(circuitBoard)
+
+	let str = ""
+	for (var yP = -20; yP < 20; yP++) {
+		for (var xP = -20; xP < 20; xP++) {
+			if (xP === 0 && yP === 0) {
+				str += "O"
+			} else {
+				const pos = circuitBoard.find(({x, y}) => x === xP && y === yP)
+				if (pos === undefined) {
+					str += "."
+				} else {
+					str += pos.nr
+				}
+			}
+		}
+		str += "\n"
+	}
+	console.log(str)
+
+	const closest = circuitBoard.filter(isIntersection).map(manhattan).reduce(smallest, Infinity)
+	console.log('\n\n2020 / 3 / Part 1\nClosest manhattan distance:', closest)
+}
+
+function reset() {
+	return { x: 0, y: 0 }
+}
+
+
+function increment(target) {
+	const xT = target.x
+	const yT = target.y
+	const pos = circuitBoard.find(({x, y}) => x === xT && y === yT)
+
+	if (pos === undefined) {
+		circuitBoard.push({ x: xT, y: yT, nr: 1 })
+	} else {
+		pos.nr++
+	}
+}
+
+function isIntersection(x) {
+	return x && x.nr > 1
+}
+
+function manhattan({ x, y }) {
+	return Math.abs(x) + Math.abs(y)
+}
+
+function smallest(acc, curr) {
+	return curr < acc ? curr : acc
+}
