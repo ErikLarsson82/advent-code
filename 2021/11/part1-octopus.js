9b536c7b0547cb45fa41d455cca9ae6d2d792eef
Erik Larsson <erik.mattias.larsson@gmail.com>
2021-12-11 13:22:58 +0100

    Solved exmple

---
+++ 2021/11/part1-octopus.js
@@ -0,0 +1,174 @@
+const example = [
+	'5483143223',
+	'2745854711',
+	'5264556173',
+	'6141336146',
+	'6357385478',
+	'4167524645',
+	'2176841721',
+	'6882881134',
+	'4846848554',
+	'5283751526',
+]
+
+/*
+const example2 = [
+	'11111',
+	'19991',
+	'19191',
+	'19991',
+	'11111'
+]
+*/
+
+const width = 10
+const height = 10
+let grid = new Array(width).fill().map(() => new Array(height).fill(null))
+
+function forEachInGrid(_grid, f) {
+	_grid.forEach((something, xIdx) => {
+		something.forEach((item, yIdx) => {
+			f(item, { x: xIdx, y: yIdx })
+		})
+	})
+}
+
+function mapGrid(f) {
+	return grid.map(something => {
+		return something.map(item => {
+			return f(item)
+		})
+	})
+}
+
+function neighbours(x, y) {
+	return [
+		{ x: x-1, y: y-1 },
+		{ x,      y: y-1 },
+		{ x: x+1, y: y-1 },
+		{ x: x-1, y },
+		// { x, y },
+		{ x: x+1, y },
+		{ x: x-1, y: y+1 },
+		{ x,      y: y+1 },
+		{ x: x+1, y: y+1 }
+	]
+}
+
+function getValue({ x, y }) {
+	return grid[x] && grid[x][y]
+}
+
+function countFlashers() {
+	let flashes = 0
+	forEachInGrid(grid, num => {
+		if (num === 'FLASH') {
+			flashes++
+		}
+	})
+	return flashes
+}
+
+function findFlasher(grid) {
+	let flasher = -1
+	forEachInGrid(grid, (num, location) => {
+		if (num === 'FLASH') {
+			flasher = location
+		}
+	})
+	return flasher
+}
+
+example.forEach((row, yIdx) => {
+	row.split('').forEach((char, xIdx) => {
+		grid[xIdx][yIdx] = char
+	})
+})
+
+
+grid = mapGrid(x => x !== null && parseInt(x) || null)
+
+/*
+console.log(grid[0][0] === 5)
+console.log(grid[1][0] === 4)
+console.log(grid[0][1] === 2)
+console.log(grid[9][9] === 6)
+printNeighbours(neighbours(0, 0).map(getValue))
+printNeighbours(neighbours(9, 9).map(getValue))
+printNeighbours(neighbours(8, 8).map(getValue))
+*/
+
+function printNeighbours(arr) {
+	let str = ''
+	const [topLeft, topMiddle, topRight, centerLeft, centerRight, bottomLeft, bottomMiddle, bottomRight] = arr.map(x => x === undefined ? '-' : x.toString())
+	str = topLeft + topMiddle + topRight + '\n' + centerLeft + ' ' + centerRight + '\n' + bottomLeft + bottomMiddle + bottomRight
+	console.log(str + '\n')
+}
+
+function printGrid() {
+	let str = ''
+	for (let i = 0; i < grid.length; i++) {
+		for (let j = 0; j < grid[i].length; j++) {
+			if (grid[j][i] === 'FLASH') {
+				str += '*'
+			} else if (grid[j][i] === 'SPENT') {
+				str += '.'
+			} else {
+				str += grid[j][i]
+			}
+		}
+		if (i < grid.length-1) str += '\n'
+	}
+	return str
+}
+
+function simulate() {
+	let flashes = 0
+	let stopper = 0
+	grid = mapGrid(num => {
+		if (typeof num === 'number') {
+			return num + 1
+		} else {
+			return num
+		}
+	})
+	grid = mapGrid(num => num > 9 ? 'FLASH' : num)
+	
+	let flasher = findFlasher(grid)
+	while (flasher !== -1 && stopper < 1000) {
+		flashes++
+		stopper++
+		neighbours(flasher.x, flasher.y).forEach(item => {
+			if (grid[item.x] && grid[item.x][item.y] && typeof grid[item.x][item.y] === 'number') {
+				grid[item.x][item.y] += 1
+			}
+		})
+		grid = mapGrid(num => num > 9 ? 'FLASH' : num)
+		grid[flasher.x][flasher.y] = 'SPENT'
+		flasher = findFlasher(grid)
+	}
+	grid = mapGrid(num => {
+		if (num === 'SPENT') {
+			return 0
+		} else {
+			return num
+		}
+	})
+
+	return flashes
+}
+
+console.log('Initial state')
+console.log(printGrid())
+console.log('')
+
+let sumFlashes = 0
+
+for (let k = 0; k <= 100-1; k++) {
+	
+	sumFlashes += simulate()	
+	
+	console.log(`Grid after iteration ${k+1} - sum flashes so far ${sumFlashes}`)
+	console.log(printGrid())
+	console.log('')
+}
